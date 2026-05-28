07c13605a78f5fc55d4063664123408c9d15c283
Eyram Cobblah <ecobblah2@gmail.com>
2026-05-28 03:00:24 +0000

    Feature: add logging with loguru

--- backend/app/api/routes/home.py
+++ backend/app/api/routes/home.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
+from app.core.logging import get_logger
+
+logger = get_logger()
 
 router = APIRouter(prefix="/home",)
 
 @router.get("/")
 def home():
+    logger.info("Home accessed")
+    logger.debug("Home accessed")
+    logger.error("Home accessed")
+    logger.warning("Home accessed")
+    logger.critical("Home accessed")
     return {"message": "Welcome to NextGen Banking API"}
--- backend/app/main.py
+++ backend/app/main.py
@@ -10,5 +10,4 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
